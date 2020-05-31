908bf4d49d4aaa5b064af1df972ccd8d0d68db20
Utsav Patel <utsavpatel91@gmail.com>
2020-05-31 23:57:06 +0530

    Updates

---
+++ precache-manifest.bdf4e54ad8a2df2bfc9b9d31a5623c30.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5bfe626cfb727f7bf4f989d7126c80e",
+    "url": "/task3/index.html"
+  },
+  {
+    "revision": "d9191119c573f416bc3c",
+    "url": "/task3/static/css/main.8ed529b5.chunk.css"
+  },
+  {
+    "revision": "3ba6a60057d377ee0031",
+    "url": "/task3/static/js/2.46f1e31c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/task3/static/js/2.46f1e31c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9191119c573f416bc3c",
+    "url": "/task3/static/js/main.baf120c9.chunk.js"
+  },
+  {
+    "revision": "4253e7e1853a9c9fcb1b",
+    "url": "/task3/static/js/runtime-main.a7210775.js"
+  }
+]);
